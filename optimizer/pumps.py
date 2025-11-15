36521283b05067d2286ab0c111e4c5fbad13035e
Valter Kraemer <valterkraemer@users.noreply.github.com>
2025-11-15 18:09:04 +0200

    chore(optimizer): add pumps.py

---
+++ optimizer/pumps.py
@@ -0,0 +1,7 @@
+def small_pump(h: float) -> [float, float]:
+    # Return power in kW and flow in m3/h
+    return [185, 1500]
+  
+def big_pump(h: float) -> [float, float]:
+    # Return power in kW and flow in m3/h
+    return [350, 3000]
